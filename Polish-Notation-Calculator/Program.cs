b274d7b5b8ee1a7fa14a4567bbd2c9490105a911
alexander-vasilets <69071341+alexander-vasilets@users.noreply.github.com>
2020-10-24 21:34:27 +0300

    Add files via upload

---
+++ Polish-Notation-Calculator/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Polish_Notation_Calculator
+{
+    class Program
+    {
+        private static Stack<string> stack = new Stack<string>();
+        private static Dictionary<string, double> vars = new Dictionary<string, double>();
+        private static bool exception = false;
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            while (line.Length != 0)
+            {
+                var words = from s in line.Split(' ') where s.Length != 0 select s;
+                foreach (string w in words)
+                {
+                    try
+                    {
+                        double res = 0, l = 0, r = 0;
+                        switch (w)
+                        {
+                            case "=":
+                                PopValue(out double v);
+                                string key = stack.Pop();
+                                vars[key] = v;
+                                stack.Push(v.ToString());
+                                break;
+                            case "+":
+                                PopValues(out r, out l);
+                                res = l + r;
+                                stack.Push(res.ToString());
+                                break;
+                            case "-":
+                                PopValues(out r, out l);
+                                res = l - r;
+                                stack.Push(res.ToString());
+                                break;
+                            case "*":
+                                PopValues(out r, out l);
+                                res = l * r;
+                                stack.Push(res.ToString());
+                                break;
+                            case "/":
+                                PopValues(out r, out l);
+                                res = l / r;
+                                stack.Push(res.ToString());
+                                break;
+                            case "%":
+                                PopValues(out r, out l);
+                                res = l % r;
+                                stack.Push(res.ToString());
+                                break;
+                            case "pow":
+                                PopValues(out r, out l);
+                                res = Math.Pow(l, r);
+                                stack.Push(res.ToString());
+                                break;
+                            case "root":
+                                PopValues(out r, out l);
+                                res = Math.Pow(l, 1 / r);
+                                stack.Push(res.ToString());
+                                break;
+                            default:
+                                stack.Push(w);
+                                break;
+                        }
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(ex.Message + "\nPress any key to continue");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        exception = true;
+                        break;
+                    }
+                }
+                if (!exception)
+                {
+                    Console.WriteLine(stack.Pop());
+                }
+                exception = false;
+                line = Console.ReadLine();
+            }
+            Console.Clear();
+            Console.WriteLine("Press any key to exit the app");
+            Console.ReadKey(true);
+        }
+        static void PopValues(out double r, out double l)
+        {
+            string right = stack.Pop();
+            if (!double.TryParse(right, out r) && !vars.TryGetValue(right, out r))
+            {
+                throw new InvalidOperationException($"Variable {right} doesn't exist");
+            }
+            string left = stack.Pop();
+            if (!double.TryParse(left, out l) && !vars.TryGetValue(left, out l))
+            {
+                throw new InvalidOperationException($"Variable {left} doesn't exist");
+            }
+        }
+        static void PopValue(out double v)
+        {
+            string value = stack.Pop();
+            if (!double.TryParse(value, out v) && !vars.TryGetValue(value, out v))
+            {
+                throw new InvalidOperationException($"Variable {value} doesn't exist");
+            }
+        }
+    }
+}
---
+++ Polish-Notation-Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Polish-Notation-Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Polish-Notation-Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ef7e7a10-ca2a-4bc7-8997-945bf1bb99be")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
